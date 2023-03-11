1e70e212e42dffbe2cdf22d69c869b533dc1a77e
unknown <giancarmelo.pittala@gmail.com>
2023-03-11 18:37:11 +0100

    fix: page hoverflow bug fix

--- components/base/Page.vue
+++ components/base/Page.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="container mx-auto px-4 xl:px-8 pb-20 lg:pb-24 gap-20 lg:gap-24 grid">
+  <section class="container overflow-x-hidden mx-auto px-4 xl:px-8 pb-20 lg:pb-24 gap-20 lg:gap-24 grid">
     <slot />
   </section>
 </template>
